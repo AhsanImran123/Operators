7c000062a13c8f5061e101279d279d4c71f4977e
AhsanImran123 <159993664+AhsanImran123@users.noreply.github.com>
2024-03-14 22:43:34 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,59 @@
+// operators +,-,*,/
+// let num1: number = 2;
+// let num2: number = 2;
+
+// let sum : number = num1 + num2
+
+// console.log(sum)
+
+// pre increment
+
+// let a : number =5;
+// let b : number =2;
+
+// console.log(a++)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+
+// post increment
+
+// let a : number =5;
+// console.log(++a)
+// console.log(a)
+
+
+// let a : number =5;
+// console.log(--a)
+// console.log(a)
+
+// let num1 : number = 5
+// let num2 : number = 8
+
+// let preincrementNumber = ++num1
+// console.log(preincrementNumber)
+
+// let num1 : number = 5
+// let num2 : number = 8
+// let num3 : number = 11
+// let postincrementNumber = ++num3
+// console.log(postincrementNumber)
+
+
+// let num1: number = "2";
+// let num2: number = 2
+
+// let sum : number = num1 + num2
+
+// console.log(sum)
+
+
+
+
+// let num1 : number = 5;
+// let num2 : number = 4;
+
+// let sum : number = num1 ** num2
+// console.log(sum)
+
+
